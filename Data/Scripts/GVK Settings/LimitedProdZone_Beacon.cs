eecd4f7b0e755d04d51cebdc36665c462ac14fae
Mike Dude <62247634+Blaylock1988@users.noreply.github.com>
2024-01-07 10:39:01 -0700

    Revert "removed out of date limits"
    This reverts commit 916a9c519abdb6d7d88197b54794b0830fc5c00f.

--- Data/Scripts/GVK Settings/LimitedProdZone_Beacon.cs
+++ Data/Scripts/GVK Settings/LimitedProdZone_Beacon.cs
@@ -40,6 +40,7 @@ namespace LimitedProdZone
             beacon = (Entity as IMyBeacon);
             LimitedProdZone_Assembler.beaconList.Add(beacon);
             LimitedProdZone_Refinery.beaconList.Add(beacon);
+            LimitedProdZone_SafeZoneBlock.beaconList.Add(beacon);
             LimitedProdZone_SmallGatlingGun.beaconList.Add(beacon);
             LimitedProdZone_LargeGatlingTurret.beaconList.Add(beacon);
             LimitedProdZone_LargeMissileTurret.beaconList.Add(beacon);
@@ -47,6 +48,7 @@ namespace LimitedProdZone
             LimitedProdZone_SmallMissileLauncherReload.beaconList.Add(beacon);
             LimitedProdZone_InteriorTurret.beaconList.Add(beacon);
             LimitedProdZone_ConveyorSorter.beaconList.Add(beacon);
+			LimitedProdZone_UpgradeModule.beaconList.Add(beacon);
 			LimitedProdZone_StaticDrill.beaconList.Add(beacon);
             if (beacon != null)
             {
@@ -153,6 +155,10 @@ namespace LimitedProdZone
             {
                 LimitedProdZone_Refinery.beaconList.Remove(beacon);
             }
+            if (LimitedProdZone_SafeZoneBlock.beaconList.Contains(beacon))
+            {
+                LimitedProdZone_SafeZoneBlock.beaconList.Remove(beacon);
+            }
             if (LimitedProdZone_SmallGatlingGun.beaconList.Contains(beacon))
             {
                 LimitedProdZone_SmallGatlingGun.beaconList.Remove(beacon);
@@ -176,6 +182,10 @@ namespace LimitedProdZone
             if (LimitedProdZone_ConveyorSorter.beaconList.Contains(beacon))
             {
                 LimitedProdZone_ConveyorSorter.beaconList.Remove(beacon);
+            }
+            if (LimitedProdZone_UpgradeModule.beaconList.Contains(beacon))
+            {
+                LimitedProdZone_UpgradeModule.beaconList.Remove(beacon);
             }
             if (LimitedProdZone_StaticDrill.beaconList.Contains(beacon))
             {
