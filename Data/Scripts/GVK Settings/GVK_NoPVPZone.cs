26846511e4fba58d274a455afb7579406f1133ba
Mike Dude <62247634+Blaylock1988@users.noreply.github.com>
2024-01-21 12:55:47 -0700

    Improved nopvpzone to include ship grinders and weapons

--- Data/Scripts/GVK Settings/GVK_NoPVPZone.cs
+++ Data/Scripts/GVK Settings/GVK_NoPVPZone.cs
@@ -21,10 +21,12 @@ namespace StarterGrinder
     {
         IMySlimBlock reuse_slim;
         IMyAngleGrinder reuse_grinder;
+        IMyShipGrinder ship_grinder;
         IMyFaction reuse_faction;
         IMyFaction reuse_faction_grinder;
         string grinder_sub = "AngleGrinder";
         float multiplier = 1.2f;
+        readonly MyStringHash grindHash = MyStringHash.GetOrCompute("Grind");
 
         // New variables for no damage/grinding in desinated area
         private Vector3D NO_DAMAGE_AREA = new Vector3D(62495, 28019, 37195); //[Coordinates:{X:62495.55 Y:28019.04 Z:37195.71}]
@@ -51,37 +53,69 @@ namespace StarterGrinder
         {
             try
             {
-                if (info.Type.ToString() == "Grind")
+                if (info.Type.Equals(grindHash))
                 {
                     reuse_slim = null;
                     reuse_slim = target as IMySlimBlock;
-                    reuse_grinder = MyAPIGateway.Entities.GetEntityById(info.AttackerId) as IMyAngleGrinder;
-                    if (reuse_grinder == null || reuse_slim == null) return;
+                    if (reuse_slim == null) return;
+
+                    IMyEntity ent = MyAPIGateway.Entities.GetEntityById(info.AttackerId);
+                    if (ent == null) return;
+
+                    reuse_grinder = null;
+                    ship_grinder = null;
+                    reuse_grinder = ent as IMyAngleGrinder;
+                    ship_grinder = ent as IMyShipGrinder;
 
                     // Checks if in the desinated zone and DOESN"T allow hacking other blocks
                     if (IsInZone(reuse_slim))
                     {
-                        long owner = reuse_slim.CubeGrid.BigOwners.FirstOrDefault();
-                        if (owner == 0) return;
-                        if (owner == reuse_grinder.OwnerIdentityId) return;
-                        //if (reuse_slim.OwnerId == 0 || reuse_slim.BuiltBy == 0) return;
-                        //if (reuse_slim.OwnerId == reuse_grinder.OwnerIdentityId) return;
-                        //if (reuse_slim.BuiltBy == reuse_grinder.OwnerIdentityId) return;
-
-                        reuse_faction = null;
-                        reuse_faction_grinder = null;
-
-                        reuse_faction = MyAPIGateway.Session.Factions.TryGetPlayerFaction(owner);
-                        reuse_faction_grinder = MyAPIGateway.Session.Factions.TryGetPlayerFaction(reuse_grinder.OwnerIdentityId);
-
-                        if (reuse_faction != null && reuse_faction_grinder != null)
+                        if (reuse_grinder != null)
                         {
-                            if (reuse_faction == reuse_faction_grinder) return;
-                            if (reuse_faction.IsEveryoneNpc() || reuse_faction.Tag.Length >= 4) return;
+                            long owner = reuse_slim.CubeGrid.BigOwners.FirstOrDefault();
+                            if (owner == 0) return;
+                            if (owner == reuse_grinder.OwnerIdentityId) return;
+                            //if (reuse_slim.OwnerId == 0 || reuse_slim.BuiltBy == 0) return;
+                            //if (reuse_slim.OwnerId == reuse_grinder.OwnerIdentityId) return;
+                            //if (reuse_slim.BuiltBy == reuse_grinder.OwnerIdentityId) return;
+
+                            reuse_faction = null;
+                            reuse_faction_grinder = null;
+
+                            reuse_faction = MyAPIGateway.Session.Factions.TryGetPlayerFaction(owner);
+                            reuse_faction_grinder = MyAPIGateway.Session.Factions.TryGetPlayerFaction(reuse_grinder.OwnerIdentityId);
+
+                            if (reuse_faction != null && reuse_faction_grinder != null)
+                            {
+                                if (reuse_faction == reuse_faction_grinder) return;
+                                //if (reuse_faction.IsEveryoneNpc() || reuse_faction.Tag.Length >= 4) return;
+                            }
+
+                            info.Amount = 0f;
+                            return;
                         }
 
-                        info.Amount = 0f;
-                        return;
+                        if (ship_grinder != null)
+                        {
+                            long owner = reuse_slim.CubeGrid.BigOwners.FirstOrDefault();
+                            if (owner == 0) return;
+                            if (owner == ship_grinder.OwnerId) return;
+
+                            reuse_faction = null;
+                            reuse_faction_grinder = null;
+
+                            reuse_faction = MyAPIGateway.Session.Factions.TryGetPlayerFaction(owner);
+                            reuse_faction_grinder = MyAPIGateway.Session.Factions.TryGetPlayerFaction(ship_grinder.OwnerId);
+
+                            if (reuse_faction != null && reuse_faction_grinder != null)
+                            {
+                                if (reuse_faction == reuse_faction_grinder) return;
+                                //if (reuse_faction.IsEveryoneNpc() || reuse_faction.Tag.Length >= 4) return;
+                            }
+
+                            info.Amount = 0f;
+                            return;
+                        }
                     }
 
                     // This checks if using the basic grinder to NOT allow hacking of other blocks
